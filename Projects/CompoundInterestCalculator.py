eb94b9ebcdd193d01027fa699b816c99705eaf34
Itkitt <itkittkun@gmail.com>
2026-09-16 04:11:13 +0800

    Edited CompoundInterestCalculator project

--- Projects/CompoundInterestCalculator.py
+++ Projects/CompoundInterestCalculator.py
@@ -11,16 +11,22 @@ while principle == 0: # While principle is 0
     principle = float(input("Enter your principle amount: ")) # Inputs principle as float
     if principle <= 0: # If principle is still less than or equal to 0
         print("Principle can't be less than or equal to 0.") # Error message
+    else:
+        break # Ensures no repetition errors
 
 while interest_rate == 0:
     interest_rate = float(input("Enter your interest rate: "))
     if interest_rate <0:
         print("Interest rate cannot be less than or equal to 0.")
+    else:
+        break
         
 while time == 0:
     time = int(input("Enter the time in years: ")) # Int because year
     if principle <= 0:
         print("Years can't be less than or equal to 0.")
+    else: 
+        break
         
 total = principle * pow((1 + interest_rate / 100), time) # Compound interest formula; principle x (1 + interest/100)^time
 
@@ -28,3 +34,4 @@ total = principle * pow((1 + interest_rate / 100), time) # Compound interest for
 print(f"Your principle is ${principle:.2f}\nYour interest rate is {interest_rate:.2f}%\nYour time in years is {time}")
 print(f"With principle (${principle:.2f}) multiplied by 1 + interest rate ({interest_rate:.2f}%), with the power of years ({time})...")
 print(f"Your compound interest would be: ${total:.2f}")
+print(f"You would be required to pay an interest of ${total - principle:.2f}") # Helps user in counting the interest
